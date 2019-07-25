c0a07be237a8c436ffa198a44ed6a0a2cb6dec64
monkeyphilip <monkey.philip32@gmail.com>
2019-07-25 13:02:31 -0400

    added scroll and font

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import SearchBox from './SearchBox';
 import CardList from './CardList';
-import { robots } from './robots';
+import Scroll from './Scroll';
+import './App.css';
+import './index.css'
 
 
 
@@ -9,9 +11,15 @@ class App extends Component {
 	constructor() {
 		super()
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: ''
 		}
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response=> response.json())
+			.then(users=> this.setState({ robots: users }));
 	}
 
 	onSearchChange = (event) => {
@@ -22,14 +30,20 @@ class App extends Component {
 		const filteredRobots = this.state.robots.filter(robots =>{
 			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 		} )
-		return (
-		<div className = 'tc'>
-			<h1>RoboFriends</h1> 
-			<SearchBox searchChange={this.onSearchChange}/>
-			<CardList robots={filteredRobots} />
-		</div>
+		if (this.state.robots.length ===0) {
+			return<h1>Loading</h1> 
+		} else {
+			return (
+			<div className = 'tc'>
+				<h1 className='f1'> RoboFriends</h1> 
+				<SearchBox searchChange={this.onSearchChange}/>
+				<Scroll>
+					<CardList robots={filteredRobots} />
+				</Scroll>
+			</div>
 
-		);
+			);
+		}
 	}
 	
 }
